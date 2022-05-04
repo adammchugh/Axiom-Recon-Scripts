94dd612bc5b614e5a64477f192ee283d77372d0e
Adam McHugh <34958726+adammchugh@users.noreply.github.com>
2022-05-04 22:29:37 +0930

    Update enum-rootdomain-to-services.sh

--- enum-rootdomain-to-services.sh
+++ enum-rootdomain-to-services.sh
@@ -21,12 +21,12 @@ axiom-scan ipaddrs.txt -m nmap -oX nmap.xml -T4 --top-ports 100 -sV
 
 python3 ports.py nmap.xml | anew hostnames.txt
 
-axiom-scan hostnames.txt -m httpx -o http_hosts.txt
+#axiom-scan hostnames.txt -m httpx -o http_hosts.txt
 
-axiom-scan http_hosts.txt -m wpscan &
+#axiom-scan http_hosts.txt -m wpscan &
 
-axiom-scan http_hosts.txt -m gobuster-dir -o http_hosts_dirbuster.txt &
+#axiom-scan http_hosts.txt -m gobuster-dir -o http_hosts_dirbuster.txt &
 
-axiom-scan http_hosts.txt -m gowitness -o screenshots &
+#axiom-scan http_hosts.txt -m gowitness -o screenshots &
 
-axiom-scan http_hosts.txt -m ffuf -o content.csv --threads 2 &
+#axiom-scan http_hosts.txt -m ffuf -o content.csv --threads 2 &
